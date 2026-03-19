6410ce835cc6cc4b34d053fadea80b47a18201f1
Claiton Nazaret <claitonnazaret@gmail.com>
2026-03-19 16:17:08 -0300

    feat: setup organization better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import { sendPasswordResetEmail, sendVerificationEmail } from './email/resend';
 import prisma from './prisma';
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL || 'http://localhost:3000',
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
